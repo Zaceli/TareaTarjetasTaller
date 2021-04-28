4385bfb70ac35d41e7a8df91e66d9dbf380a05ae
GitAches3000 <94sebash@gmail.com>
2021-04-27 23:50:13 -0400

    color Ramdom

--- index.js
+++ index.js
@@ -7,6 +7,16 @@
     document.body.style.background =color;
     document.querySelector('generator').textContent = color;
 }*/
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }    
+    document.getElementById("colorpad").style.background = color;
+    document.getElementById("buton").style.background = color;    
+  }
 const createCardNode = ()=>{
     const imagenContainer = document.createElement("img")
     imagenContainer.className= "image-container"
