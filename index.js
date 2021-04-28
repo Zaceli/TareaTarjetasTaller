adbfbf4556864453ef5d5fcbc2543af540b5f762
GitAches3000 <94sebash@gmail.com>
2021-04-28 19:15:08 -0400

    cambio de Fondo

--- index.js
+++ index.js
@@ -17,31 +17,38 @@ function getRandomColor() {
     document.getElementById("colorpad").style.background = color;
     document.getElementById("buton").style.background = color;    
   }
+  
+  
 const createCardNode = ()=>{
+
     const imagenContainer = document.createElement("img")
     imagenContainer.className= "image-container"
-
+    
     const box = document.createElement("div")
     box.className = "box"
-
+    //box.background = getRandomColor()
+    
     const oboton = document.createElement("button")
     oboton.className = "bota"
+    
     const textob = document.createTextNode('Learn More')
     oboton.appendChild(textob)
-
+    
     const title = document.createElement("h1")
     title.className = "titulo"
+    
     const textot = document.createTextNode('SEDANS')
     title.appendChild(textot)
-
+    
     const parrafo = document.createElement("p")
     parrafo.className = "description"
+    
     const text = document.createTextNode('Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.')
     parrafo.appendChild(text)
-
+    
     const card = document.createElement("div")
     card.className = "card"
-    
+   
     card.appendChild(title)
     card.appendChild(box)
     card.appendChild(parrafo)
@@ -50,11 +57,14 @@ const createCardNode = ()=>{
 
     return card
 }
+
 const mountNode = document.querySelector("#appe")
 const addCardButton = document.querySelector('button') 
 
+
 const addCard=()=>{
     mountNode.appendChild(createCardNode())
+    
 }
 
 addCardButton.addEventListener('click', addCard)
